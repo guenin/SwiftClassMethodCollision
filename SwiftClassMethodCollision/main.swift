bd6216f65e70be01c6f2e7ba72877e514410ac64
John Guenin <john@guen.in>
2015-02-04 09:05:08 -0800

    Initial Commit

---
+++ SwiftClassMethodCollision/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftClassMethodCollision
+//
+//  Created by John Guenin on 2/4/15.
+//  Copyright (c) 2015 John Guenin. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
